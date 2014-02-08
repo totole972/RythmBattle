da93199ac4d7604e3149bd0b825de0162305373e
nathanael.bertrand <nbertrand@doc4web.com>
2014-02-08 13:19:46 +0100

    Suppression des implementations vides

--- src/com/Game1/rythmbattle/game/implementations/GameManager.java
+++
@@ -1,9 +0,0 @@
-package com.Game1.rythmbattle.game.implementations;
-
-import com.Game1.rythmbattle.game.interfaces.IGameManager;
-
-/**
- * Created by totole on 07/02/14.
- */
-public class GameManager implements IGameManager {
-}
--- src/com/Game1/rythmbattle/game/implementations/Timeline.java
+++
@@ -1,9 +0,0 @@
-package com.Game1.rythmbattle.game.implementations;
-
-import com.Game1.rythmbattle.game.interfaces.ITimeline;
-
-/**
- * Created by totole on 07/02/14.
- */
-public class Timeline implements ITimeline {
-}
