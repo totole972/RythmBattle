dacb71d07e15f91aea6eb030676103e5f26e50e0
nathanael.bertrand <nbertrand@doc4web.com>
2014-02-08 12:40:36 +0100

    Suppression des impplementations des listeners: a implementer a la volée

--- src/com/Game1/rythmbattle/listener/implementations/ItemTouchListener.java
+++
@@ -1,9 +0,0 @@
-package com.Game1.rythmbattle.listener.implementations;
-
-import com.Game1.rythmbattle.listener.interfaces.IItemTouchListener;
-
-/**
- * Created by totole on 07/02/14.
- */
-public class ItemTouchListener implements IItemTouchListener {
-}
